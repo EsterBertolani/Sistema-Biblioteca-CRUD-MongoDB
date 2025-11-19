37fc0793190da3204065c025793170b1971e619f
EsterBertolani <esterdasilvabertolani@gmail.com>
2025-11-19 10:19:52 -0300

    alt Leitor{}

--- Modelo_Nao_Relacional/src/model/leitor.py
+++ Modelo_Nao_Relacional/src/model/leitor.py
@@ -1,48 +1,46 @@
-class Leitor:
-    def __init__(self, 
-                 id_leitor: int = None,
-                 nome: str = None,
-                 cpf: str = None,
-                 telefone: str = None,
-                 email: str = None):
-        self.set_id_leitor(id_leitor)
-        self.set_nome(nome)
-        self.set_cpf(cpf)
-        self.set_telefone(telefone)
-        self.set_email(email)
+from bson import ObjectId
 
-    # GETTERS E SETTERS
-    def set_id_leitor(self, id_leitor: int):
-        self.id_leitor = id_leitor
-    def get_id_leitor(self) -> int:
-        return self.id_leitor
-    
-    def set_nome(self, nome: str):
+class Leitor:
+    def __init__(self,
+                _id: ObjectId = None, #  no Mongo o _id padrão é um ObjectId. Usar ObjectId() garante IDs únicos e compatibilidade direta com drivers (pymongo).
+                 nome: str = "",
+                 cpf: str = "",
+                 telefone: str = "",
+                 email: str = ""):
+        
+        self._id = _id if _id else ObjectId()
         self.nome = nome
-    def get_nome(self) -> str:
-        return self.nome
-    
-    def set_cpf(self, cpf: str):
         self.cpf = cpf
-    def get_cpf(self) -> str:
-        return self.cpf
-    
-    def set_telefone(self, telefone: str):
         self.telefone = telefone
-    def get_telefone(self) -> str:
-        return self.telefone
-    
-    def set_email(self, email: str):
         self.email = email
-    def get_email(self) -> str:
-        return self.email
-    
-    # REPRESENTAÇÃO EM STRING
-    def to_string(self) -> str:
+
+    # ----------------------------------------
+    def to_dict(self): # converte o objeto em um dicionário para facilitar a inserção
+        return {
+            "_id": self._id,
+            "nome": self.nome,
+            "cpf": self.cpf,
+            "telefone": self.telefone,
+            "email": self.email
+        }
+
+    # ----------------------------------------
+    @staticmethod
+    def from_dict(dados: dict): # cria um objeto leitor a partir de um dicionário recebido do banco
+        return Leitor(
+            _id=dados.get("_id"),
+            nome=dados.get("nome"),
+            cpf=dados.get("cpf"),
+            telefone=dados.get("telefone"),
+            email=dados.get("email")
+        )
+
+    # ----------------------------------------
+    def to_string(self):
         return (
-            f"ID Leitor: {self.get_id_leitor()} | "
-            f"Nome: {self.get_nome()} | "
-            f"CPF: {self.get_cpf()} | "
-            f"Telefone: {self.get_telefone()} | "
-            f"Email: {self.get_email()}"
+            f"ID Leitor: {self._id} | "
+            f"Nome: {self.nome} | "
+            f"CPF: {self.cpf} | "
+            f"Telefone: {self.telefone} | "
+            f"Email: {self.email}"
         )
